9e9db741d57569ad8b88c4873094ad809d229d29
Baturpcu <batusalkim11@gmail.com>
2022-12-27 18:14:21 +0300

    last update

--- myproject/App.js
+++ myproject/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   const [name, setName] = useState("");
@@ -7,24 +7,25 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Enter Name : {name}</Text>
-      
 
+      <Text>Enter Your Name :{name} </Text>
       <TextInput
       
-      style={styles.input}
-      placeholder="enter your name "
-      onChangeText={(val)=> setName(val)}
+        style={styles.input}
+        placeholder="enter your name"
+        onChangeText={(degisken) => setName(degisken)}
       />
-      <Text>Enter Age : {age}</Text>
 
+      <Text>Enter Your Age : {age}</Text>
       <TextInput
-      keyboardType="numeric"
-      style={styles.input}
-      placeholder="enter your age"
-      onChangeText={(batu)=> setAge(batu)}/>  
+        keyboardType="numeric"
+        style={styles.input}
+        placeholder="enter your age "
+        onChangeText={(NewAgeDegisken)=>setAge(NewAgeDegisken)}
+        
+      />
+      
     </View>
-    
   );
 }
 
@@ -35,14 +36,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input:{
-    borderWidth:1,
-    borderColor:"#777",
-    padding:8,
-    margin:10,
-    width:150,
-    backgroundColor:"white",
-    color:"black",
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 150,
   }
 
 });
