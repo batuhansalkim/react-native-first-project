6f1ffe760a222bada9030fa3b2cbac299784a6ee
Baturpcu <batusalkim11@gmail.com>
2023-01-04 02:16:07 +0300

    last update

---
+++ myproject/components/sandbox.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet,Text,View } from "react-native";
+
+export default function Sandbox(){
+    return(
+        <Text>sandbox</Text>
+    )
+}
+
+const style = StyleSheet.create({
+
+});
+
+
+
+
+
+
