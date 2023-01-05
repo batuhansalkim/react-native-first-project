bcfbda64993e26e998245085be8dc0f3e0631915
Baturpcu <batusalkim11@gmail.com>
2023-01-05 19:19:40 +0300

    last update

--- myproject/App.js
+++ myproject/App.js
@@ -71,18 +71,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:"#320356",
     color:"#ccc"
 
   },
   content:{
     padding:40,
-    backgroundColor:"black",
     flex:1,
   },
   list:{
     flex:1,
     marginTop:20,
-    backgroundColor:"yellow",
   }
 })
