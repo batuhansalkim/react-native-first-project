8330096816c437b6ac4aa34f4e5607630248b071
Baturpcu <batusalkim11@gmail.com>
2023-01-06 15:53:16 +0300

    last update

--- gamezone/App.js
+++ gamezone/App.js
@@ -22,7 +22,7 @@ export default function App() {
     return(
       <AppLoading
       startAsync={getFonts}
-      onFinish={()=> setFont}
+      onFinish={()=> setFontsLoaded(true)}
     />
     )
     
