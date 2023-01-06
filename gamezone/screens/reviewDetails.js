a02b6d9e418ac5c2a2e7c9f6f0ba1ef1e4de3edc
Baturpcu <batusalkim11@gmail.com>
2023-01-06 13:53:05 +0300

    last update

--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -1,7 +1,19 @@
+import React from "react";
+import { StyleSheet,View,Text } from "react-native";
 
+export default function ReviewDetails(){
+    return(
+        <View style={styles.container}>
+            <Text>ReviewDetails Screen</Text>
+        </View>
+    )
+}
 
-
-
+const styles = StyleSheet.create({
+    container:{
+        
+    }
+})
 
 
 
