bab1d0205be09e455f3b333c4e681890e5a435ce
Baturpcu <batusalkim11@gmail.com>
2023-01-06 15:03:17 +0300

    last update

--- gamezone/App.js
+++ gamezone/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import * as Font from "expo-font";
 import Home from './screens/home';
 
+const getFonts = () =>{
+  return Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf")
+  })
+}
 
 export default function App() {
   return (
     <Home/>
-    
+
   );
 }
 
