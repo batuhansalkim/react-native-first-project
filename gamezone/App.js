c619847732442d6d9e83ea53cade5a8c292c9e1e
Baturpcu <batusalkim11@gmail.com>
2023-01-07 12:45:55 +0300

    last update

--- gamezone/App.js
+++ gamezone/App.js
@@ -1,35 +1,22 @@
 import React, { useState } from "react";
-import * as Font from "expo-font";
-import Home from './screens/home';
-import {AppLoading} from 'expo';
-
-const getFonts = () =>Font.loadAsync({
-    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
-    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
-
-  })
+import {  StyleSheet, Text } from "react-native";
 
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false); 
-
-
-  if(fontsLoaded){
-    return (
-      <Home/>
-    );
-  }
-  else{
-    return(
-      <AppLoading
-      startAsync={getFonts}
-      onFinish={()=> setFontsLoaded(true)}
-    />
-    )
-    
-  }
+  return(
+    <Text style={styles.title}>Hello</Text>
+  )
   
 }
 
-
+const styles = StyleSheet.create({
+  title:{
+    fontSize:20,
+    color:"white",
+    backgroundColor:"black",
+    textAlign:"center",
+    justifyContent:"center",
+    paddingTop:40,
+  }
+})
 
