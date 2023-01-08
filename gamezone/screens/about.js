dd09f33a62be67b353907f45ed48f2907217405d
Baturpcu <batusalkim11@gmail.com>
2023-01-08 13:11:30 +0300

    last update

---
+++ gamezone/screens/about.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet,View,Text } from "react-native";
+
+
+export default function About(){
+    return(
+        <View style = {styles.container}>
+            <Text></Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        padding:24,
+    }
+})
+
+
+
+
+
+
+
+
+
+
