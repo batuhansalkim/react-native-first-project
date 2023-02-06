497a7a6cc7200fd1011879636caf5a8ec4c56d2b
Baturpcu <batusalkim11@gmail.com>
2023-02-06 22:16:59 +0300

    update

--- gamezone/App.js
+++ gamezone/App.js
@@ -1,22 +1,54 @@
 import React, { useState } from "react";
-import {  StyleSheet, Text,View } from "react-native";
-
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-  return(
-    <View style={styles.container}>
-      <Text>open up App. js to start working on your app!</Text>
-    </View>
-  )
-  
+  const [name, setName] = useState("batuhan");
 }
+return (
+  <View style={styles.container}>
+    <Text>My name is {name}</Text>
+  </View>
+)
+// export default function App() {
+//   const [name, setName] = useState("shaun");
+//   const [person, setPerson] = useState({name: "mario", age:40});
 
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:"#fff",
-    alignItems:"center",
-    justifyContent:"center",
-  }
-})
+//   const clickHandler = () => {
+//     setName("batuhan");
+//     setPerson({name: "degisti", age: 22})
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <Text>My name is {name}</Text>
+//       <Text>His name is {person.name} and his age is {person.age}</Text>
+//       <View style={styles.buttonContainer}>
+//         <Button title="isim degistir" onPress={clickHandler } />
+//       </View>
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   buttonContainer: {
+//     marginTop: 20,
+//   }
+// })
+
+
+
+
+
+
+
+
+
+
+
 
