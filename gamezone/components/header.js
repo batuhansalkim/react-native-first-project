984ddbe996abc920634e2f1b4fd529d3e54d99a0
Baturpcu <batusalkim11@gmail.com>
2023-02-13 19:02:53 +0300

    update

--- gamezone/components/header.js
+++ gamezone/components/header.js
@@ -1,8 +1,17 @@
 import React, {useState} from "react";
 import { StyleSheet,Text, View, FlatList } from "react-native";
 
+export default function Header(){
+    return(
+        <View style={styles.headaer}>
+            <Text style={styles.title}>My Todos</Text>
+        </View>
+    )
+}
 
+const styles = StyleSheet.create({
 
+})
 
 
 
