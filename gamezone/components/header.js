8480351ac4848aaadb6ded5babb83e53b2313357
Baturpcu <batusalkim11@gmail.com>
2023-02-13 19:18:02 +0300

    update

--- gamezone/components/header.js
+++ gamezone/components/header.js
@@ -10,7 +10,11 @@ export default function Header(){
 }
 
 const styles = StyleSheet.create({
-
+    header:{
+        height:80,
+        paddingTop:38,
+        backgroundColor:"coral"
+    }
 })
 
 
